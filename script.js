7d59a037d76bab6cef784a03502e005c11095c59
teenSPT <neosis0101@gmail.com>
2025-01-17 19:23:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -33,21 +33,7 @@ const spreadData = {
     'XAUUSD_TITAN': 30,
 };
 
-// 特殊文字をエスケープする関数
-function escapeHTML(str) {
-    return str.replace(/[&<>"']/g, function (match) {
-        const escape = {
-            '&': '&amp;',
-            '<': '&lt;',
-            '>': '&gt;',
-            '"': '&quot;',
-            "'": '&#39;'
-        };
-        return escape[match];
-    });
-}
-
-// 選択された値を管理する変数
+// 各選択肢の初期値
 let selectedTicker = defaultValues.ticker;
 let selectedTime = defaultValues.time;
 let selectedBlock = defaultValues.block;
@@ -64,109 +50,108 @@ let selectedC3tm = defaultValues.c3tm;
 let selectedC3nc = defaultValues.c3nc;
 let selectedBroker = defaultValues.broker;
 
-// 各更新関数
-function updateTicker(value) {
-    console.log('Tickerが更新されました:', value);
-    selectedTicker = value;
+// 特殊文字をエスケープする関数
+function escapeHTML(str) {
+    if (typeof str !== "string") return str;
+    return str.replace(/[&<>"']/g, function (match) {
+        const escape = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;'
+        };
+        return escape[match];
+    });
 }
 
+// 各選択肢の更新関数
+function updateTicker(value) {
+    selectedTicker = value;
+}
 function updateTime(value) {
     selectedTime = value;
 }
-
 function updateBlock(value) {
     selectedBlock = value;
 }
-
 function updateB2tm(value) {
     selectedB2tm = value;
 }
-
 function updateB2blk(value) {
     selectedB2blk = value;
 }
-
 function updateTfc(value) {
     selectedTfc = value;
 }
-
 function updateDecisiveTime(value) {
     selectedDecisiveTime = value;
 }
-
 function updateDecisiveBlock(value) {
     selectedDecisiveBlock = value;
 }
-
 function updateC1tm(value) {
     selectedC1tm = value;
 }
-
 function updateC1nc(value) {
     selectedC1nc = value;
 }
-
 function updateC2tm(value) {
     selectedC2tm = value;
 }
-
 function updateC2nc(value) {
     selectedC2nc = value;
 }
-
 function updateC3tm(value) {
     selectedC3tm = value;
 }
-
 function updateC3nc(value) {
     selectedC3nc = value;
 }
-
 function updateBroker(value) {
     selectedBroker = value;
 }
 
-// 選択内容を表示する関数
+// 選択内容を実行して結果を表示する関数
 function executeSelection() {
     const resultDiv = document.getElementById('result');
     const key = `${selectedTicker}_${selectedBroker}`;
     const spread = spreadData[key] !== undefined ? spreadData[key] : "N/A";
 
+    // 結果表示のための情報を構築
     const resultInfo = `
         ${selectedTicker !== defaultValues.ticker ? escapeHTML(selectedTicker) : ''}: 
-        ${selectedTime !== defaultValues.time ? selectedTime : ''} 
-        ${selectedBlock !== defaultValues.block ? selectedBlock : ''}   
-        ${selectedB2tm !== defaultValues.b2tm ? selectedB2tm : ''} 
-        ${selectedB2blk !== defaultValues.b2blk ? selectedB2blk : ''} / 
-        TFC: ${selectedTfc !== defaultValues.tfc ? selectedTfc : ''} / 
-        決定打: ${selectedDecisiveTime !== defaultValues.decisiveTime ? selectedDecisiveTime : ''} 
-        ${selectedDecisiveBlock !== defaultValues.decisiveBlock ? selectedDecisiveBlock : ''} / 
-        ${selectedC1tm !== defaultValues.c1tm ? selectedC1tm : ''} 
-        ${selectedC1nc !== defaultValues.c1nc ? selectedC1nc : ''} / 
-        ${selectedC2tm !== defaultValues.c2tm ? selectedC2tm : ''} 
-        ${selectedC2nc !== defaultValues.c2nc ? selectedC2nc : ''} / 
-        ${selectedC3tm !== defaultValues.c3tm ? selectedC3tm : ''} 
-        ${selectedC3nc !== defaultValues.c3nc ? selectedC3nc : ''}...
-        ${selectedBroker !== defaultValues.broker ? selectedBroker : ''} Spread: ${spread}
-    `.replace(/\s+/g, ' ').trim();
+        ${selectedTime !== defaultValues.time ? escapeHTML(selectedTime) : ''} 
+        ${selectedBlock !== defaultValues.block ? escapeHTML(selectedBlock) : ''}   
+        ${selectedB2tm !== defaultValues.b2tm ? escapeHTML(selectedB2tm) : ''} 
+        ${selectedB2blk !== defaultValues.b2blk ? escapeHTML(selectedB2blk) : ''} / 
+        TFC: ${selectedTfc !== defaultValues.tfc ? escapeHTML(selectedTfc) : ''} / 
+        決定打: ${selectedDecisiveTime !== defaultValues.decisiveTime ? escapeHTML(selectedDecisiveTime) : ''} 
+        ${selectedDecisiveBlock !== defaultValues.decisiveBlock ? escapeHTML(selectedDecisiveBlock) : ''} / 
+        ${selectedC1tm !== defaultValues.c1tm ? escapeHTML(selectedC1tm) : ''} 
+        ${selectedC1nc !== defaultValues.c1nc ? escapeHTML(selectedC1nc) : ''} / 
+        ${selectedC2tm !== defaultValues.c2tm ? escapeHTML(selectedC2tm) : ''} 
+        ${selectedC2nc !== defaultValues.c2nc ? escapeHTML(selectedC2nc) : ''} / 
+        ${selectedC3tm !== defaultValues.c3tm ? escapeHTML(selectedC3tm) : ''} 
+        ${selectedC3nc !== defaultValues.c3nc ? escapeHTML(selectedC3nc) : ''}...
 
-    resultDiv.innerText = resultInfo;
+        ${selectedBroker !== defaultValues.broker ? escapeHTML(selectedBroker) : ''} Spread: ${spread}
+    `;
+
+    resultDiv.innerText = resultInfo.replace(/\s+/g, ' ').trim();
 }
 
-// コピー機能
+// コピーする関数
 function copyInfo() {
     const resultDiv = document.getElementById('result');
     const textToCopy = resultDiv.innerText.trim();
 
-    if (textToCopy) {
-        navigator.clipboard.writeText(textToCopy)
-            .then(() => alert('Copy‼'))
-            .catch(err => alert('Failed to copy: ' + err));
-    } else {
-        alert('Nothing to copy!');
-    }
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => alert('Copy‼'))
+        .catch(err => alert('Failed to copy: ' + err));
 }
 
+// スプレッドをコピーする関数
 function copySpread() {
     const key = `${selectedTicker}_${selectedBroker}`;
     const spread = spreadData[key] !== undefined ? spreadData[key] : "N/A";
